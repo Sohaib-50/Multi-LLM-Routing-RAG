0346c6abcc508732079681028c4e181de8b2d5ef
Sohaib-50 <sohaibahmedabbasi2@gmail.com>
2024-08-19 14:39:01 +0500

    removed language based semantic routes

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -17,52 +17,52 @@ SEMANTIC_ROUTES = [
             "Ciao",
         ],
     ),
-    SemanticRoute(
-        name="urdu",
-        llm_type=LLMType.STRONG,
-        utterances=[
-            "Mujhe HR policies ki details explain karo",
-            "Kya programming seekhna asaan hai?",
-            "میں نے اس کی تلاش کی",
-            "میں نے اس کو دیکھا",
-            "میں نے اسے دیکھا",
-            "ہوا میں تازگی کا احساس بہت خوشگوار تھا۔",
-            "کل شام ہم نے دوستوں کے ساتھ ایک خوبصورت کیفے میں کھانا کھایا۔",
-            "کتابوں کا مطالعہ انسان کی عقل و فہم کو بڑھاتا ہے۔",
-            "بچوں کو خوبصورت خوابوں کی تعلیم دینا ضروری ہے۔",
-            "پہاڑی علاقے میں رہنے والے لوگ عموماً بہت مہمان نواز ہوتے ہیں۔",
-        ]
-    ),
-    SemanticRoute(
-        name="arabic",
-        llm_type=LLMType.STRONG,
-        utterances=[
-            "ما هي سياسة الموارد البشرية لديكم؟",
-            "هل تعتقد أن تعلم البرمجة سهل؟",
-            "أنا أبحث عنه",
-            "رأيته",
-            "رأيتها",
-            "كانت الطقس جميلة بالأمس",
-            "أمس في المساء، تناولنا العشاء في مقهى جميل مع الأصدقاء",
-            "قراءة الكتب تعزز العقل والفهم لدى الإنسان",
-            "تعليم الأطفال الأحلام الجميلة ضروري",
-            "الناس الذين يعيشون في المناطق الجبلية عادة ما يكونون مضيافين جدا",
-            'Qira’at al-kutub tu’azziz min dhaka’ al-insan wa fahmuhu',
-            'Hal min al-sahl ta’allum al-barmajah?',
-            'Ashkhas al-manatiq al-jabaliyya ‘aadatan ma yakunun dayafina jiddan',
-        ]
-    ),
-    SemanticRoute(
-        name="multi_lingual",
-        llm_type=LLMType.STRONG,
-        utterances=[
-            "kiya programming seekhna easy hai?",
-            "mujhe HR policies ki details explain karo",
-            "Can you tell me about the 'Parlez-vous anglais?'",
-            "¿Dónde está el 'supermercado'?",
-            "我想知道 'weather' 的中文是什么?",
-        ],
-    ),
+    # SemanticRoute(
+    #     name="urdu",
+    #     llm_type=LLMType.STRONG,
+    #     utterances=[
+    #         "Mujhe HR policies ki details explain karo",
+    #         "Kya programming seekhna asaan hai?",
+    #         "میں نے اس کی تلاش کی",
+    #         "میں نے اس کو دیکھا",
+    #         "میں نے اسے دیکھا",
+    #         "ہوا میں تازگی کا احساس بہت خوشگوار تھا۔",
+    #         "کل شام ہم نے دوستوں کے ساتھ ایک خوبصورت کیفے میں کھانا کھایا۔",
+    #         "کتابوں کا مطالعہ انسان کی عقل و فہم کو بڑھاتا ہے۔",
+    #         "بچوں کو خوبصورت خوابوں کی تعلیم دینا ضروری ہے۔",
+    #         "پہاڑی علاقے میں رہنے والے لوگ عموماً بہت مہمان نواز ہوتے ہیں۔",
+    #     ]
+    # ),
+    # SemanticRoute(
+    #     name="arabic",
+    #     llm_type=LLMType.STRONG,
+    #     utterances=[
+    #         "ما هي سياسة الموارد البشرية لديكم؟",
+    #         "هل تعتقد أن تعلم البرمجة سهل؟",
+    #         "أنا أبحث عنه",
+    #         "رأيته",
+    #         "رأيتها",
+    #         "كانت الطقس جميلة بالأمس",
+    #         "أمس في المساء، تناولنا العشاء في مقهى جميل مع الأصدقاء",
+    #         "قراءة الكتب تعزز العقل والفهم لدى الإنسان",
+    #         "تعليم الأطفال الأحلام الجميلة ضروري",
+    #         "الناس الذين يعيشون في المناطق الجبلية عادة ما يكونون مضيافين جدا",
+    #         'Qira’at al-kutub tu’azziz min dhaka’ al-insan wa fahmuhu',
+    #         'Hal min al-sahl ta’allum al-barmajah?',
+    #         'Ashkhas al-manatiq al-jabaliyya ‘aadatan ma yakunun dayafina jiddan',
+    #     ]
+    # ),
+    # SemanticRoute(
+    #     name="multi_lingual",
+    #     llm_type=LLMType.STRONG,
+    #     utterances=[
+    #         "kiya programming seekhna easy hai?",
+    #         "mujhe HR policies ki details explain karo",
+    #         "Can you tell me about the 'Parlez-vous anglais?'",
+    #         "¿Dónde está el 'supermercado'?",
+    #         "我想知道 'weather' 的中文是什么?",
+    #     ],
+    # ),
 
 ]
 
